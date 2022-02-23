1f531053583b60e378854860b96f2a1a0be0f76a
SamsonLoML <samlomanlap@gmail.com>
2022-02-23 08:11:24 +0000

    Fix typo in introduction to user guide

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 # from .models import related models
-
+from .model import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
+    extra = 5
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['name']
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = [['name', 'type', 'make', 'year']
+    inlines = [CarModelInline]
 # Register models here
+admin.site.register(CarModelInline, CarModelAdmin, CarMakeAdmin)
+admin.site.register(CarMake)
+admin.site.register(CarModel)
